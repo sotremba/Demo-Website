69a43f721e9e35c1fafc010235715223eb93ac13
Stephen Otremba <sotremba007@gmail.com>
2020-06-15 16:09:08 -0700

    implemented user sign in and registration

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from flask import Flask, request, render_template
-from google.oauth2 import id_token
-from google.auth.transport import requests
 import sqlite3
 
 app = Flask(__name__)
@@ -24,108 +22,135 @@ def close_db(conn):
 def create_user_table():
     """Creates a 'users' table in our database IF ONE DOES NOT ALREADY EXIST"""
     conn, c = open_db()
-    c.execute('''CREATE TABLE IF NOT EXISTS users (user_id text, given_name text, family_name text, email text);''')
+    c.execute('''CREATE TABLE IF NOT EXISTS users (given_name text, family_name text, email text, state text,                                                               username text, password text);''')
     close_db(conn)
 
 
-def insert_user(user_id, given_name, family_name, email):
+def insert_user(given_name, family_name, email, state, username, password):
     """Inserts a user into the 'users' table of the database"""
     conn, c = open_db()
-    c.execute('''INSERT INTO users VALUES (?,?,?,?);''', (user_id, given_name, family_name, email))
+    c.execute('''INSERT INTO users VALUES (?,?,?,?,?,?);''', (given_name, family_name, email, state, username,                                                                      password))
     close_db(conn)
 
 
-def is_user_registered(user_id):
-    """Determines if a given user id is already registered in our database"""
+def is_email_registered(email):
+    """Determines if a given email is already registered in our database"""
     conn, c = open_db()
-    user = c.execute('''SELECT * FROM users WHERE user_id=?;''', (user_id,)).fetchone()
+    user_email = c.execute('''SELECT * FROM users WHERE email=?;''', (email,)).fetchone()
+    close_db(conn)
+    return user_email != None
+
+
+def is_username_registered(username):
+    """Determines if a given username is already registered in our database"""
+    conn, c = open_db()
+    user = c.execute('''SELECT * FROM users WHERE username=?;''', (username,)).fetchone()
     close_db(conn)
     return user != None
 
 
-@app.route('/button', methods=['GET', 'POST'])
-def button_home_page():
-    """Display the home page of the site that uses a traditional sign-in button"""
-    print("Button Home Page")
-    return render_template('button_home.html')
+def is_password_correct(username, password):
+    """Determines if a given password matches the stored password for a particular username. 
+    The username must be in the database"""
+    conn, c = open_db()
+    stored_password = c.execute('''SELECT password FROM users WHERE username=?;''', (username,)).fetchone()
+    close_db(conn)
+    print(stored_password)
+    return password == stored_password[0]
 
 
-@app.route('/onetap', methods=['GET', 'POST'])
-def onetap_home_page():
-    """Display the home page of the site that uses OneTap sign-in"""
-    print("OneTap Home Page")
-    return render_template('onetap_home.html')
+def get_first_name(username):
+    """Gets the first name of the user with the given username. The username must be in the database"""
+    conn, c = open_db()
+    name = c.execute('''SELECT given_name FROM users WHERE username=?;''', (username,)).fetchone()
+    close_db(conn)
+    return name[0]
 
 
-@app.route('/button-token', methods=['GET', 'POST'])
-def handle_user():
-    """Handle a user request by signing them in if they are an existing user or registering them otherwise"""
-    print('Handling User Request')
-    token = request.values.get('id_token')
-    user_info = verify_id_token(token, CLIENT_ID)
+@app.route('/', methods=['GET'])
+def home_page():
+    """Display the home page of the site"""
+    print("Demo home page")
+    return render_template('home.html')
 
-    if user_info == "INVALID TOKEN":
-        print(user_info)
-        return render_template('invalid_id.html')
 
+@app.route('/register', methods=['GET'])
+def display_register():
+    """Display the user registration page of the site"""
+    print("Registration page")
+    return render_template('register.html')
+
+
+@app.route('/register-user', methods=['POST'])
+def register_new_user():
+    """Register a new user with the given information"""
+    print("Registering User")
+    fname = request.values.get('fname')
+    lname = request.values.get('lname')
+    email = request.values.get('email').lower()
+    state = request.values.get('state')
+    username = request.values.get('usr')
+    password = request.values.get('passwrd')
+    #should really encrypt and salt passwords, but this is just a demo
+    
     create_user_table()
-    registered = is_user_registered(user_info['sub'])
-    if registered:
-        return render_template('returning_user.html', name=user_info['given_name'])
+    registered_email = is_email_registered(email)
+    registered_username = is_username_registered(username)
+    
+    if registered_email:
+        #send them back to a login/registration page
+        print("Reg Failed: Existing Email")
+        return render_template('register_existing_email.html')
+    
+    if registered_username:
+        #send them back to a login/registration page
+        print("Reg Failed: Existing Username")
+        return render_template('register_existing_username.html')
+        
     else:
-        user_id = user_info['sub']
-        given_name = user_info['given_name']
-        family_name = user_info['family_name']
-        email = user_info['email']
-        insert_user(user_id, given_name, family_name, email) 
-        return render_template('new_user.html', name=given_name)
+        print("Registering New User")
+        insert_user(fname, lname, email, state, username, password)
+        return render_template('registration_success.html', name=fname)
 
 
-@app.route('/onetap-token', methods=['GET', 'POST'])
-def handle_onetap():
-    """Handle signing in when Google sends the token to our server directly"""
-    print('Handling User Request from OneTap')
-    #Verify CSRF doubel submit cookie
-    csrf_token_cookie = request.cookies.get('g_csrf_token')
-    if not csrf_token_cookie:
-        return "No CSRF token in Cookie"
-    csrf_token_body = request.cookies.get('g_csrf_token')
-    if not csrf_token_body:
-        return "No CSRF token in post body"
-    if csrf_token_body != csrf_token_cookie:
-        return "Failed to verify double submit cookie"
-
-    token = request.values.get('credential')
-    user_info = verify_id_token(token, CLIENT_ID)
-
-    if user_info == "INVALID TOKEN":
-        print(user_info)
-        return render_template('invalid_id.html')
-
-    create_user_table()
-    registered = is_user_registered(user_info['sub'])
-    if registered:
-        return render_template('returning_user.html', name=user_info['given_name'])
-    else:
-        user_id = user_info['sub']
-        given_name = user_info['given_name']
-        family_name = user_info['family_name']
-        email = user_info['email']
-        insert_user(user_id, given_name, family_name, email) 
-        return render_template('new_user.html', name=given_name)
+@app.route('/login', methods=['GET'])
+def display_login():
+    """Display the user login page of the site"""
+    print("Login page")
+    return render_template('login.html')
 
 
-def verify_id_token(token, client_id):
-    """Verify that a given id_token is valid and return the decoded user information if it is valid"""
-    print("Begin Token Verification")
-    try:
-        idinfo = id_token.verify_oauth2_token(token, requests.Request(), client_id)
-        if idinfo['iss'] not in ['accounts.google.com', 'https://accounts.google.com']:
-            raise ValueError("Wrong Issuer")
+@app.route('/login-user', methods=['POST'])
+def login_existing_user():
+    """Login an existing user with the given information"""
+    print("Logging in User")
+    username = request.values.get('usr')
+    password = request.values.get('passwrd')
+    
+    registered_username = is_username_registered(username)
+    if not registered_username:
+        return render_template('login_missing_username.html')
+    
+    correct_password = is_password_correct(username, password)
+    if correct_password:
+        first_name = get_first_name(username)
+        return render_template('login_success.html', name=first_name)
+    
+    return render_template('incorrect_password.html')
+        
+    
 
-        return idinfo
 
-    except ValueError:
-        return "INVALID TOKEN"
+        
 
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
